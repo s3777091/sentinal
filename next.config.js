6284d87fa26c9b65be21270be8ffca93946e9808
annguyen1909 <nguyentruongan1419@gmail.com>
2024-08-16 23:59:38 +0700

    update communities 1.0

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
     eslint: {
       // Warning: This allows production builds to successfully complete even if
       // your project has ESLint errors.
