8bbd4d2942e233a3799cb5fa9b82245f4a697c00
s3777091 <s3777091@rmit.edu.vn>
2024-08-25 13:21:09 +0700

    back end

---
+++ cyberBackend/src/get.ts
@@ -0,0 +1,55 @@
+import { CyberReceive } from "@dad1909/cyber";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+dotenv.config();
+
+const psw = process.env.KAFKA_PASSWORD;
+if (!psw) {
+  throw new Error("PASSWORD must be set");
+}
+
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+async function main() {
+  const cyberReceive = new CyberReceive(psw!);
+
+  const handleMessage = async (message: {
+    username: string;
+    title: string;
+    level: string;
+    detail: string;
+  }) => {
+    console.log("Received message:");
+    // Find the user by username
+    const user = await prisma.user.findUnique({
+      where: {
+        username: message.username,
+      },
+      cacheStrategy: { swr: 60, ttl: 60 },
+    });
+
+    // If the user is found, update the ScanData table
+    if (user) {
+      await prisma.scanData.create({
+        data: {
+          title: message.title,
+          detail: message.level,
+          more_detail: message.detail,
+          userId: user.id,
+        },
+      });
+      console.log(`ScanData entry created for user ${message.username}`);
+    } else {
+      console.error(`User with username ${message.username} not found`);
+    }
+  };
+
+  await cyberReceive.getMessage(handleMessage);
+}
+
+// Run the main
+main().catch((error) => {
+  console.error("Error in main function:", error);
+});
---
+++ cyberBackend/src/index.ts
@@ -0,0 +1,27 @@
+import { Client } from "@gradio/client";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const hfToken: string | undefined = process.env.HUGGINGFACE_API_KEY;
+const baseModel: string | undefined = process.env.BASE_ADMIN_MODEL;
+if (!hfToken || !hfToken.startsWith("hf_")) {
+  throw new Error(
+    "HUGGINGFACE_API_KEY is either not defined or not in the correct format (must start with 'hf_')."
+  );
+}
+
+if (!baseModel) {
+  throw new Error("pls adding baseModel in .env or .env.local");
+}
+
+async function main() {
+  const app = await Client.connect("dad1909/cyberapi_1", {
+    hf_token: hfToken as `hf_${string}`,
+  });
+
+}
+
+main().catch((error) => {
+  console.error("Error in main function:", error);
+});
